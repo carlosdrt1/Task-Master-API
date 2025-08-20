9cff5dd66a50a6a559b73d625177c8b29dbd485a
Carlos Duarte <carlosduarteinfo@gmail.com>
2025-08-20 20:35:12 -0300

    feat: create HasherService and export with SharedModule

---
+++ src/shared/hasher/hasher.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@nestjs/common';
+import * as argon2 from 'argon2';
+
+@Injectable()
+export class HashService {
+  async hash(text: string): Promise<string> {
+    return argon2.hash(text, {
+      type: argon2.argon2id,
+    });
+  }
+
+  async verify(hash: string, text: string) {
+    return argon2.verify(hash, text);
+  }
+}
