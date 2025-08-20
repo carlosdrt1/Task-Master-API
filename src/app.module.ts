5d8eb43de09562e2730a4f0501652f9e0cef6e43
Carlos Duarte <carlosduarteinfo@gmail.com>
2025-08-20 19:35:32 -0300

    feat: add UserModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
+import { UsersModule } from './modules/users/users.module';
 import * as Joi from 'joi';
 
 @Module({
@@ -11,6 +12,7 @@ import * as Joi from 'joi';
         DATABASE_URL: Joi.string().uri().required(),
       }),
     }),
+    UsersModule,
   ],
   controllers: [AppController],
   providers: [AppService],
