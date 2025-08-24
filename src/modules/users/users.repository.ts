6d0a479e0a6b71ccbcebb3e1cce6c70fadf9f90d
Carlos Duarte <carlosduarteinfo@gmail.com>
2025-08-24 15:57:02 -0300

    feat(user): add functions to implement OAuth login

--- src/modules/users/users.repository.ts
+++ src/modules/users/users.repository.ts
@@ -6,18 +6,28 @@ import { UpdateUserDto } from './dto/update-user.dto';
 import { PrismaService } from '@/database/prisma/prisma.service';
 import { Provider } from '@/shared/enums/provider.enum';
 import { plainToInstance } from 'class-transformer';
-
-type UserInput = CreateUserDto & {
-  provider: Provider;
-};
+import { CreateUserOAuthDto } from './dto/create-user-oauth.dto';
 
 @Injectable()
-export class UserRepository implements AbstractRepository<User> {
+export class UsersRepository implements AbstractRepository<User> {
   constructor(private readonly prismaService: PrismaService) {}
 
-  async create(data: UserInput): Promise<User> {
-    const user = await this.prismaService.user.create({ data });
+  async create(data: CreateUserDto): Promise<User> {
+    const user = await this.prismaService.user.create({
+      data: {
+        ...data,
+        provider: Provider.APP,
+      },
+    });
     return plainToInstance(User, user);
+  }
+
+  async createUserOAuth(data: CreateUserOAuthDto): Promise<User> {
+    const user = await this.prismaService.user.create({ data });
+
+    return plainToInstance(User, user, {
+      groups: ['auth'],
+    });
   }
 
   async findById(id: string): Promise<User | null> {
@@ -27,6 +37,16 @@ export class UserRepository implements AbstractRepository<User> {
 
   async findByEmail(email: string): Promise<User | null> {
     const user = await this.prismaService.user.findUnique({ where: { email } });
+    return plainToInstance(User, user, {
+      groups: ['auth'],
+    });
+  }
+
+  async findByProviderId(providerId: string): Promise<User | null> {
+    const user = await this.prismaService.user.findUnique({
+      where: { providerId },
+    });
+
     return plainToInstance(User, user, {
       groups: ['auth'],
     });
