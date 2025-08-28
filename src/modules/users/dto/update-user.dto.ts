9e44d106f66730825438fc4c8347d115eaddd902
Carlos Duarte <carlosduarteinfo@gmail.com>
2025-08-28 20:43:06 -0300

    refactor: modify updateUserDto

--- src/modules/users/dto/update-user.dto.ts
+++ src/modules/users/dto/update-user.dto.ts
@@ -1,4 +1,6 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateUserDto } from './create-user.dto';
+import { MinLength } from 'class-validator';
 
-export class UpdateUserDto extends PartialType(CreateUserDto) {}
+export class UpdateUserDto {
+  @MinLength(3)
+  name: string;
+}
