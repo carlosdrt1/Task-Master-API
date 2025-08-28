f03f2f63bf58ffe251c623539b315af6f1dae7e7
Carlos Duarte <carlosduarteinfo@gmail.com>
2025-08-28 20:37:30 -0300

    refactor: remove findOrCreateOAuthUser method and return types to IUser

--- src/modules/users/users.service.ts
+++ src/modules/users/users.service.ts
@@ -1,40 +1,22 @@
-import { ConflictException, Injectable } from '@nestjs/common';
-import { CreateUserDto } from './dto/create-user.dto';
+import { Injectable } from '@nestjs/common';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { UsersRepository } from './users.repository';
-import { User } from './entities/user.entity';
-import { CreateUserOAuthDto } from './dto/create-user-oauth.dto';
+import { IUser } from './interfaces/user.interface';
+import { CreateUserDto } from './dto/create-user.dto';
 
 @Injectable()
 export class UsersService {
   constructor(private readonly usersRepository: UsersRepository) {}
 
-  async create(dto: CreateUserDto): Promise<User> {
+  async create(dto: CreateUserDto): Promise<IUser> {
     return this.usersRepository.create(dto);
   }
 
-  async findOrCreateOAuthUser(dto: CreateUserOAuthDto): Promise<User> {
-    const exists = await this.findByEmail(dto.email);
-    if (!exists) return this.usersRepository.createUserOAuth(dto);
-
-    if (exists.provider !== dto.provider) {
-      throw new ConflictException(
-        `This email is already registered with ${exists.provider}. Please log in using ${exists.provider}`,
-      );
-    }
-
-    return exists;
-  }
-
-  async findOne(id: string): Promise<User | null> {
+  async findOne(id: string): Promise<IUser | null> {
     return this.usersRepository.findById(id);
   }
 
-  async findByEmail(email: string): Promise<User | null> {
-    return this.usersRepository.findByEmail(email);
-  }
-
-  async update(id: string, dto: UpdateUserDto): Promise<User> {
+  async update(id: string, dto: UpdateUserDto): Promise<IUser> {
     return this.usersRepository.update(id, dto);
   }
 
