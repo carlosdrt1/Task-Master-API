57217f6977977eabc2178503db70a825579e7d34
Carlos Duarte <carlosduarteinfo@gmail.com>
2025-08-28 20:43:59 -0300

    feat: add new accounts factory and modify user factory

---
+++ test/factories/accounts.factory.ts
@@ -0,0 +1,20 @@
+import { IAccount } from '@/modules/accounts/interfaces/account.interface';
+import { Provider } from '@/shared/types/provider.type';
+import { faker } from '@faker-js/faker/.';
+
+export const accountDbFactory = (override?: Partial<IAccount>): IAccount => {
+  const provider: Provider =
+    override?.provider || faker.number.int(1) === 1 ? 'APP' : 'GOOGLE';
+
+  return {
+    id: faker.string.uuid(),
+    userId: faker.string.uuid(),
+    email: provider === 'APP' ? faker.internet.email() : null,
+    provider,
+    providerId: provider !== 'APP' ? faker.string.numeric(12) : null,
+    password: provider === 'APP' ? faker.internet.password() : null,
+    createdAt: faker.date.past(),
+    updatedAt: faker.date.recent(),
+    ...override,
+  };
+};
--- test/factories/user.factory.ts
+++ test/factories/user.factory.ts
@@ -1,49 +1,25 @@
-import { LoginDto } from '@/modules/auth/dto/login.dto';
-import { CreateUserOAuthDto } from '@/modules/users/dto/create-user-oauth.dto';
-import { CreateUserDto } from '@/modules/users/dto/create-user.dto';
-import { User } from '@/modules/users/entities/user.entity';
-import { Provider } from '@/shared/enums/provider.enum';
+import { LoginDto } from '@/modules/auth/dto/login-app.dto';
+import { RegisterAppDto } from '@/modules/auth/dto/register-app.dto';
+import { IUser } from '@/modules/users/interfaces/user.interface';
 import { faker } from '@faker-js/faker';
 
-export const userDBfactory = (override?: Partial<User>): User => {
-  const provider = override?.provider || faker.helpers.enumValue(Provider);
-
+export const userDBfactory = (override?: Partial<IUser>): IUser => {
   return {
     id: faker.string.uuid(),
     name: faker.person.fullName(),
-    email: faker.internet.email(),
-    provider: provider,
-    providerId: provider !== Provider.APP ? faker.string.uuid() : undefined,
-    password: provider === Provider.APP ? faker.internet.password() : undefined,
     createdAt: faker.date.past(),
     updatedAt: faker.date.recent(),
     ...override,
   };
 };
 
-export const createUserInputFactory = (
-  override?: Partial<CreateUserDto>,
-): CreateUserDto => {
+export const registerAppInputFactory = (
+  override?: Partial<RegisterAppDto>,
+): RegisterAppDto => {
   return {
     name: faker.person.fullName(),
     email: faker.internet.email(),
     password: faker.internet.password(),
-    ...override,
-  };
-};
-
-export const OAuthUserInputFactory = (
-  override?: Partial<CreateUserOAuthDto>,
-): CreateUserOAuthDto => {
-  const providersOAuth = Object.values(Provider).filter((p) => {
-    return p !== Provider.APP;
-  });
-
-  return {
-    name: faker.person.fullName(),
-    email: faker.internet.email(),
-    provider: faker.helpers.arrayElement(providersOAuth),
-    providerId: faker.string.numeric(12),
     ...override,
   };
 };
