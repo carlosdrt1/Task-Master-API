b65bea630cc33af2a27118f0233465ab36af2aec
Carlos Duarte <carlosduarteinfo@gmail.com>
2025-08-28 21:09:01 -0300

    test: add profileFactory to use in tests

--- test/factories/accounts.factory.ts
+++ test/factories/accounts.factory.ts
@@ -1,6 +1,7 @@
 import { IAccount } from '@/modules/accounts/interfaces/account.interface';
 import { Provider } from '@/shared/types/provider.type';
 import { faker } from '@faker-js/faker/.';
+import { Profile } from 'passport';
 
 export const accountDbFactory = (override?: Partial<IAccount>): IAccount => {
   const provider: Provider =
@@ -15,6 +16,16 @@ export const accountDbFactory = (override?: Partial<IAccount>): IAccount => {
     password: provider === 'APP' ? faker.internet.password() : null,
     createdAt: faker.date.past(),
     updatedAt: faker.date.recent(),
+    ...override,
+  };
+};
+
+export const profileFactory = (override?: Partial<Profile>): Profile => {
+  return {
+    id: faker.string.numeric(12),
+    displayName: faker.internet.displayName(),
+    provider: 'google',
+    emails: [{ value: faker.internet.email() }],
     ...override,
   };
 };
